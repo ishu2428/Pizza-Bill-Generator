3bac419dcd4b298a5fcb29fe396c5b1a49c03d68
ishu2428 <ishukumargolu@gmail.com>
2025-06-07 20:21:47 +0530

    modified

---
+++ pizza_Bill_Generator/src/com/dominoz/pizza/classes/Deluxe_Pizza.java
@@ -0,0 +1,46 @@
+package com.dominoz.pizza.classes;
+
+import com.dominoz.pizza.methods.Addon;
+import com.dominoz.pizza.methods.NonVegPizza;
+import com.dominoz.pizza.methods.VegPizza;
+
+public class Deluxe_Pizza implements Addon,NonVegPizza,VegPizza
+{
+
+	@Override
+	public int vegPizzaCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int NonVegPizzaCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int cheeseCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int topping() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int takeAway() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dinning() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ pizza_Bill_Generator/src/com/dominoz/pizza/classes/TotalBillGenerator.java
@@ -0,0 +1,10 @@
+package com.dominoz.pizza.classes;
+
+public class TotalBillGenerator {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
